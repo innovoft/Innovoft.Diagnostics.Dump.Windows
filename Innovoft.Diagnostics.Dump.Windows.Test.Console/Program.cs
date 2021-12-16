2927bb8b7f2291883e58f3357156e4adbfd600ba
Steven Retz <sretz@innovoft.com>
2021-12-16 12:42:48 -0300

    added test program Innovoft.Diagnostics.Dump.Windows.Test.Console

---
+++ Innovoft.Diagnostics.Dump.Windows.Test.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Innovoft.Diagnostics;
+
+namespace Innovoft.Diagnostics
+{
+	internal class Program
+	{
+		#region Main
+		private static int Main(string[] args)
+		{
+			Dump.Write("dump.dmp", DumpType.Normal);
+			return 0;
+		}
+		#endregion //Main
+	}
+}
