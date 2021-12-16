e7462bf5723056b161707bd6e158c29d9fe8f86e
Steven Retz <sretz@innovoft.com>
2021-12-16 12:06:07 -0300

    added class internal struct ExceptionInfo

---
+++ ExceptionInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Innovoft.Diagnostics
+{
+	[StructLayout(LayoutKind.Sequential, Pack = 4)]
+	internal struct ExceptionInfo
+	{
+		public uint ThreadId;
+		public IntPtr ExceptionPointers;
+		public int ClientPointers;
+	}
+}
